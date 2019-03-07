599849e02f2d3ed50190f332690574ac80e4c9c0
Anthony Alvarado <j.a_alvarado99@hotmail.com>
2019-03-06 21:02:36 -0600

    Creacion de proyecto + Primeros 3 métodos de la clase listaB

---
+++ TareaCorta2/TareaCorta2/TareaCorta2.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include <iostream>
+#include "ListaSimple.h"
+
+int main() {
+
+	List<int> lm("Lista1");
+	int x;
+
+	lm.push_front(30);
+	lm.push_front(20);
+	lm.push_front(10);
+	lm.push_back(40);
+	lm.insertarR(5, 0);
+	lm.insertarR(8, 1);
+	lm.insertarR(15, 5);
+	lm.insertarR(50, 99);
+	lm.insertarR(999, 1);
+
+	lm.remove(1, x);
+	lm.pop(x);
+	lm.pop_back(x);
+	//cout <<"Prueba: "<< x;
+
+
+	lm.print();
+	std::cout << "\n";
+	List<string> la("Personas");
+	la.push_back("ana");
+	la.push_back("juan");
+	la.print();
+	std::cout << "\n";
+}
